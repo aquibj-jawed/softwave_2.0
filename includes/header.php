1fa1819751dd636b28b20f7a924c677181a0dfd5
unknown <aquib_j@yahoo.com>
2024-06-24 18:20:11 +0530

    Header Added

--- includes/header.php
+++ includes/header.php
@@ -1,32 +1,83 @@
-<header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            
-            <!-- Mobile Button -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                
-                </li>
-                <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+    <header>
+
+        <div class="bg-gray-lite header-top py-3">
+            <div class="container">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <p class="mb-0 fs-6 text-dark fw-500">Call Us: <span><a href="">+919676825571</a></span> &nbsp; Email us: <span><a href="">support@softwavehub.com</a></span></p>
+                    </div>
+                    <div class="d-flex justify-content-end align-items-center gap-4">
+                        <p class="mb-0 fs-6 text-dark fw-500"><i class="bi bi-clock-fill text-primary"></i> &nbsp;08:00 am - 06:00 pm</p>
+                        <div class="mb-0 fs-6 text-dark fw-500 d-flex justify-content-center align-items-center gap-3">
+                            <a href="#"><i class="text-primary-lite bi bi-linkedin"></i></a>
+                            <a href="#"><i class="text-primary-lite bi bi-facebook"></i></a>
+                            <a href="#"><i class="text-primary-lite bi bi-instagram"></i></a>
+                            <a href="#"><i class="text-primary-lite bi bi-youtube"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
-</header>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-white py-0">
+            <div class="container-fluid ps-0">
+                <div class="bg-primary p-4">
+                    <a class="navbar-brand text-white fs-2 fw-700 mx-3" href="#">Softwavehub</a>
+                </div>
+                
+                <!-- Mobile Button -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-4">
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Services
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Service 1</a></li>
+                                <li><a class="dropdown-item" href="#">Service 2</a></li>
+                            </ul>
+                        </li>
+
+
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Portfolio</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Solutions
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Solution 1</a></li>
+                                <li><a class="dropdown-item" href="#">Solution 2</a></li>
+                            </ul>
+                        </li>
+
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Marketing
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Marketing 1</a></li>
+                                <li><a class="dropdown-item" href="#">Marketing 2</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact</a>
+                        </li>
+                        
+                    </ul>
+
+                    <a href="" class="btn btn-primary btn-lg fw-700 fs-6 py-3">Meet With Us</a>
+                </div>
+            </div>
+        </nav>
+    </header>
