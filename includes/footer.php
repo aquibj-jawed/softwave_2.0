f21146c9acc8c87ddf4ac02153f69d78806ece77
unknown <aquib_j@yahoo.com>
2024-06-25 10:54:10 +0530

    Footer Added

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,105 @@
-<!-- footer.php -->
-</main>
-<footer>
-    <!-- Footer content -->
-    <p>&copy; <?php echo date("Y"); ?> Your Website. All rights reserved.</p>
+<footer class="py-5">
+    <div class="container">
+        <div class="row justify-content-between my-4">
+
+            <div class="col-md-3">
+                <div class="d-flex gap-3 align-items-center">
+                    <div>
+                        <div class="bg-primary grid-center footer-icon">
+                            <i class="text-white bi bi-whatsapp"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-gray-200 mb-1">Whatsapp Us</p>
+                        <p class="fw-700 text-white mb-0" ><a href="">+919676825571</a></p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="d-flex gap-3 align-items-center">
+                    <div>
+                        <div class="bg-primary grid-center footer-icon">
+                            <i class="text-white bi bi-envelope"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-gray-200 mb-1">Write to us</p>
+                        <p class="fw-700 text-white mb-0" ><a href="">support@softwavehub.com</a></p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="d-flex gap-3 align-items-center">
+                    <div>
+                        <div class="bg-primary grid-center footer-icon">
+                            <i class="text-white bi bi-clock"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-gray-200 mb-1">Office hours</p>
+                        <p class="fw-700 text-white mb-0" ><a href="">Mon-Sat 9:00 - 7:00</a></p>
+                    </div>
+
+                </div>
+            </div>           
+        </div>
+
+    <br>
+
+        <div class="row justify-content-between py-3 text-white">
+            <div class="col-md-3 col-md-2 mb-3">
+                <h2 class="fw-700 text-white mb-4">Softwavehub</h2>
+                <p class="text-gray-200">At Softwavehub Solutions, we are about technology and dedicated to providing IT solutions for businesses of all sizes.</p>
+            </div>
+
+            <div class="col-md-3 col-md-2 mb-3">
+                <h2 class="fw-700 text-white mb-4">Contacts</h2>
+                <p class="text-gray-200"><i class="bi bi-clock"></i>&nbsp;  Mon-Sat 9:00 - 7:00</p>
+                <p class="text-gray-200"><i class="bi bi-phone"></i>&nbsp; +919676825571</p>
+                <p class="text-gray-200"><i class="bi bi-envelope"></i>&nbsp; support@softwavehub.com</p>
+            </div>
+
+            <div class="col-md-3 col-md-2 mb-3">
+                <form>
+                    <h2 class="fw-700 text-white mb-4">Subscribe</h2>
+                    <p>Are you interested in follow to a particular website</p>
+                    <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                        <label for="newsletter1" class="visually-hidden">Email address</label>
+                        <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                        <button class="btn btn-primary" type="submit">Subscribe</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="d-flex flex-column flex-sm-row justify-content-between pt-4 mt-4">
+            <p class="text-center text-gray-200 ">&copy; <?php echo date("Y"); ?>  Softwavehub, Inc. All rights reserved.</p>
+            <ul class="list-unstyled d-flex">
+                <li class="ms-3">
+                    <a class="link-dark" href="#">
+                        <i class=" text-gray-200 bi bi-twitter-x"></i>
+                    </a>
+                </li>
+                <li class="ms-3">
+                    <a class="link-dark" href="#">
+                        <i class=" text-gray-200 bi bi-instagram"></i>
+                    </a>
+                </li>
+                <li class="ms-3">
+                    <a class="link-dark" href="#">
+                        <i class=" text-gray-200 bi bi-facebook"></i>
+                    </a>
+                </li>
+                <li class="ms-3">
+                    <a class="link-dark" href="#">
+                        <i class=" text-gray-200 bi bi-linkedin"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </footer>
-<script src="/assets/js/script.js"></script>
-</body>
-</html>
